e0e159a8a23ab14467fb56b56aafdba52e51d029
unknown <hamamyazjy2017@gmail.com>
2020-09-09 12:29:04 +0300

    first commit

---
+++ android/app/src/main/kotlin/com/example/ecommer_admin/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.ecommer_admin
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/repo/product_repository.dart
@@ -0,0 +1,52 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:ecommer_admin/models/order_model.dart';
+import 'package:ecommer_admin/models/products.dart';
+import 'package:ecommer_admin/models/users_model.dart';
+import 'package:ecommer_admin/repo/product_client.dart';
+
+class FirebaseRepository {
+  FirebaseRepository._();
+
+  static FirebaseRepository firebaseRepository = FirebaseRepository._();
+
+  getAllOrder() async {
+    QuerySnapshot documentSnapshot =
+        await ProductClient.categoriesClient.getAllOrder();
+
+    List<OrderModel> orders = documentSnapshot.documents
+        .map((e) => OrderModel.fromDocument(e))
+        .toList();
+    return orders;
+  }
+
+  Future<List<UserModel>> getAllUsers() async {
+    QuerySnapshot documentSnapshot =
+        await ProductClient.categoriesClient.getAllUsers();
+
+    List<UserModel> products = documentSnapshot.documents
+        .map((e) => UserModel.fromDocument(e))
+        .toList();
+    // print(products);
+    return products;
+  }
+
+  Future<List<Products>> getAllProducts() async {
+    QuerySnapshot documentSnapshot =
+        await ProductClient.categoriesClient.getAllProduct();
+
+    List<Products> products = documentSnapshot.documents
+        .map((e) => Products.fromDocument(e))
+        .toList();
+    // print(products);
+    return products;
+  }
+
+  updateOrder(OrderModel orderModel, String docId) async {
+    QuerySnapshot querySnapshot = await ProductClient.categoriesClient
+        .updateOrder(orderModel.toJson(), docId);
+
+    List<OrderModel> orderModel2 =
+        querySnapshot.documents.map((e) => OrderModel.fromDocument(e)).toList();
+    return orderModel2;
+  }
+}
